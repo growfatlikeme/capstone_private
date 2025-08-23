cc6eaf8ab326c3e738a9a9e4d5d12c1f719eb7ab
GrowFatLikeMe <freakishcrap@gmail.com>
2025-08-23 18:14:15 +0800

    updated iam policy

--- terraform/iam_policy.tf
+++ terraform/iam_policy.tf
@@ -34,36 +34,3 @@ resource "aws_iam_role_policy_attachment" "node_group_elb_policy" {
   role       = module.eks.eks_managed_node_groups.learner_ng.iam_role_name
 }
 
-resource "aws_iam_role" "github_oidc" {
-  name = "github-actions-role"
-
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "sts:AssumeRoleWithWebIdentity",
-            "Principal": {
-                "Federated": "arn:aws:iam::255945442255:oidc-provider/token.actions.githubusercontent.com"
-            },
-            "Condition": {
-                "StringEquals": {
-                    "token.actions.githubusercontent.com:aud": [
-                        "sts.amazonaws.com"
-                    ]
-                },
-                "StringLike": {
-                    "token.actions.githubusercontent.com:sub": [
-                        "repo:growfatlikeme/capstone_private:*"
-                    ]
-                }
-            }
-        }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "github_oidc_policy_attach" {
-  role       = aws_iam_role.github_oidc.name
-  policy_arn = aws_iam_policy.external_dns.arn
-}
