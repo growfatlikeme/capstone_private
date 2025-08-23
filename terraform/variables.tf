e49ea499ddea969e6137bb9eddf0aa8e376854b0
GrowFatLikeMe <freakishcrap@gmail.com>
2025-08-23 19:53:55 +0800

    updated variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,31 +3,6 @@ variable "aws_account_id" {
   type        = string
 }
 
-variable "mem1" {
-  description = "Team member 1 username"
-  type        = string
-}
-
-variable "mem2" {
-  description = "Team member 2 username"
-  type        = string
-}
-
-variable "mem3" {
-  description = "Team member 3 username"
-  type        = string
-}
-
-variable "mem4" {
-  description = "Team member 4 username"
-  type        = string
-}
-
-variable "mem5" {
-  description = "Team member 5 username"
-  type        = string
-}
-
 
 variable "team_members" {
   type    = list(string)
