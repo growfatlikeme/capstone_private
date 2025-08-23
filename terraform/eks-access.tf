5babf453f7f0cd5f662a24b9a58099e8a62a0d7b
GrowFatLikeMe <freakishcrap@gmail.com>
2025-08-23 19:21:17 +0800

    changed formating of user

--- terraform/eks-access.tf
+++ terraform/eks-access.tf
@@ -1,28 +1,25 @@
+locals {
+  team_member_arns = toset([
+    format("arn:aws:iam::%s:user/%s", var.aws_account_id, var.mem1),
+    format("arn:aws:iam::%s:user/%s", var.aws_account_id, var.mem2),
+    format("arn:aws:iam::%s:user/%s", var.aws_account_id, var.mem3),
+    format("arn:aws:iam::%s:user/%s", var.aws_account_id, var.mem4),
+    format("arn:aws:iam::%s:user/%s", var.aws_account_id, var.mem5),
+  ])
+}
+
 # Grant cluster admin access to team members
 resource "aws_eks_access_entry" "team_members" {
-  for_each = toset([
-    "arn:aws:iam::${var.aws_account_id}:user/${var.mem1}",
-    "arn:aws:iam::${var.aws_account_id}:user/${var.mem2}",
-    "arn:aws:iam::${var.aws_account_id}:user/${var.mem3}",
-    "arn:aws:iam::${var.aws_account_id}:user/${var.mem4}",
-    "arn:aws:iam::${var.aws_account_id}:user/${var.mem5}",
-  ])
-
+  for_each      = local.team_member_arns
   cluster_name  = module.eks.cluster_name
   principal_arn = each.value
-  type         = "STANDARD"
+  type          = "STANDARD"
 }
+
 
 # Use policy association for admin permissions
 resource "aws_eks_access_policy_association" "team_admin_policy" {
-  for_each = toset([
-    "arn:aws:iam::${var.aws_account_id}:user/${var.mem1}",
-    "arn:aws:iam::${var.aws_account_id}:user/${var.mem2}",
-    "arn:aws:iam::${var.aws_account_id}:user/${var.mem3}",
-    "arn:aws:iam::${var.aws_account_id}:user/${var.mem4}",
-    "arn:aws:iam::${var.aws_account_id}:user/${var.mem5}",
-  ])
-
+  for_each      = local.team_member_arns
   cluster_name  = module.eks.cluster_name
   principal_arn = each.value
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
