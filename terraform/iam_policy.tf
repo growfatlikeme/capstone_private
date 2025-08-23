da1831d4c35fa9fd40be9187077a79eea9cf10a1
GrowFatLikeMe <freakishcrap@gmail.com>
2025-08-23 17:50:39 +0800

    test odic again

--- terraform/iam_policy.tf
+++ terraform/iam_policy.tf
@@ -38,23 +38,27 @@ resource "aws_iam_role" "github_oidc" {
   name = "github-actions-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Federated = "arn:aws:iam::255945442255:oidc-provider/token.actions.githubusercontent.com"
-        },
-        Action = "sts:AssumeRoleWithWebIdentity",
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          },
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:growfatlikeme/group3-sre:*"
-          }
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "sts:AssumeRoleWithWebIdentity",
+            "Principal": {
+                "Federated": "arn:aws:iam::255945442255:oidc-provider/token.actions.githubusercontent.com"
+            },
+            "Condition": {
+                "StringEquals": {
+                    "token.actions.githubusercontent.com:aud": [
+                        "sts.amazonaws.com"
+                    ]
+                },
+                "StringLike": {
+                    "token.actions.githubusercontent.com:sub": [
+                        "repo:growfatlikeme/capstone_private:*"
+                    ]
+                }
+            }
         }
-      }
     ]
   })
 }
