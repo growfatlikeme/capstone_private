b45182e80d97032675fbf124826dae6f6b96c35a
kevinczkm <kevinc.zkm@gmail.com>
2025-09-04 11:58:42 +0800

    comment out the old externaldns

--- terraform/iam_policy.tf
+++ terraform/iam_policy.tf
@@ -1,30 +1,30 @@
-resource "aws_iam_policy" "external_dns" {
-  name_prefix = "${local.name_prefix}-external-dns"
-  description = "External DNS policy for Route53 access"
+# resource "aws_iam_policy" "external_dns" {
+#   name_prefix = "${local.name_prefix}-external-dns"
+#   description = "External DNS policy for Route53 access"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "route53:ChangeResourceRecordSets"
-        ]
-        Resource = [
-          ["arn:aws:route53:::hostedzone/*"]
-        ]
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "route53:ListHostedZones",
-          "route53:ListResourceRecordSets"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "route53:ChangeResourceRecordSets"
+#         ]
+#         Resource = [
+#           ["arn:aws:route53:::hostedzone/*"]
+#         ]
+#       },
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "route53:ListHostedZones",
+#           "route53:ListResourceRecordSets"
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
 
 
 # Attach ELB policy to node group for LoadBalancer creation
